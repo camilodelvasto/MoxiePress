0482482eb6772df34c89918cff6b5dd198920cb9
Camilo Delvasto <camilodelvasto@gmail.com>
2016-01-28 19:11:37 -0500

    improved naming and added json file

--- moxie-press.php
+++ moxie-press.php
@@ -15,14 +15,14 @@ Text Domain:  moxie-press
 
 
 // initialize plugin
-add_action( 'init', 'register_cpt_movie_collection' );
-add_action( 'init', 'register_movie_custom_fields' );
+add_action( 'init', 'moxie_press_register_cpt_movie_collection' );
+add_action( 'init', 'moxie_press_register_movie_custom_fields' );
 
 // set text-domain var
 define('TEXT_DOMAIN', 'moxie-press');
 
 // register cpt with mostly default labels and args
-function register_cpt_movie_collection() {
+function moxie_press_register_cpt_movie_collection() {
 
   // set default labels and args for this cpt
   $labels = array(
@@ -65,12 +65,12 @@ function register_cpt_movie_collection() {
 }
 
 // register meta box for holding custom fields
-function register_movie_custom_fields(){
+function moxie_press_register_movie_custom_fields(){
 
   // add meta box
-  add_action( 'add_meta_boxes', 'moxie_movies_add_meta_box' );
+  add_action( 'add_meta_boxes', 'moxie_press_add_meta_box' );
 
-  function moxie_movies_get_meta( $value ) {
+  function moxie_press_get_meta( $value ) {
     global $post;
 
     $field = get_post_meta( $post->ID, $value, true );
@@ -81,11 +81,11 @@ function register_movie_custom_fields(){
     }
   }
 
-  function moxie_movies_add_meta_box() {
+  function moxie_press_add_meta_box() {
     add_meta_box(
-      'moxie_movies-moxie-movies',
-      __( 'Moxie Movies', 'moxie_movies' ),
-      'moxie_movies_html',
+      'moxie_press-moxie-movies',
+      __( 'Moxie Movies', 'moxie_press' ),
+      'moxie_press_html',
       'movie',
       'normal',
       'default'
@@ -93,60 +93,60 @@ function register_movie_custom_fields(){
   }
 
   // write the html for the editor
-  function moxie_movies_html( $post) {
-    wp_nonce_field( '_moxie_movies_nonce', 'moxie_movies_nonce' ); ?>
+  function moxie_press_html( $post) {
+    wp_nonce_field( '_moxie_press_nonce', 'moxie_press_nonce' ); ?>
 
     <p>
-      <label for="moxie_movies_poster_url"><b><?php _e( 'Poster URL', TEXT_DOMAIN ); ?></b><br>
+      <label for="moxie_press_poster_url"><b><?php _e( 'Poster URL', TEXT_DOMAIN ); ?></b><br>
       <i>Paste here the URL of the poster image for this movie</i></label><br>
-      <input type="text" name="moxie_movies_poster_url" id="moxie_movies_poster_url" value="<?php echo moxie_movies_get_meta( 'moxie_movies_poster_url' ); ?>">
+      <input type="text" name="moxie_press_poster_url" id="moxie_press_poster_url" value="<?php echo moxie_press_get_meta( 'moxie_press_poster_url' ); ?>">
     </p>
 
     <p>
-      <label for="moxie_movies_rating"><b><?php _e( 'Rating', TEXT_DOMAIN ); ?></b><br>
+      <label for="moxie_press_rating"><b><?php _e( 'Rating', TEXT_DOMAIN ); ?></b><br>
       <i>Movie rating, from 1 to 5</i></label><br>
-      <select name="moxie_movies_rating" id="moxie_movies_rating">
-        <option <?php echo (moxie_movies_get_meta( 'moxie_movies_rating' ) === '1' ) ? 'selected' : '' ?>>1</option>
-        <option <?php echo (moxie_movies_get_meta( 'moxie_movies_rating' ) === '2' ) ? 'selected' : '' ?>>2</option>
-        <option <?php echo (moxie_movies_get_meta( 'moxie_movies_rating' ) === '3' ) ? 'selected' : '' ?>>3</option>
-        <option <?php echo (moxie_movies_get_meta( 'moxie_movies_rating' ) === '4' ) ? 'selected' : '' ?>>4</option>
-        <option <?php echo (moxie_movies_get_meta( 'moxie_movies_rating' ) === '5' ) ? 'selected' : '' ?>>5</option>
+      <select name="moxie_press_rating" id="moxie_press_rating">
+        <option <?php echo (moxie_press_get_meta( 'moxie_press_rating' ) === '1' ) ? 'selected' : '' ?>>1</option>
+        <option <?php echo (moxie_press_get_meta( 'moxie_press_rating' ) === '2' ) ? 'selected' : '' ?>>2</option>
+        <option <?php echo (moxie_press_get_meta( 'moxie_press_rating' ) === '3' ) ? 'selected' : '' ?>>3</option>
+        <option <?php echo (moxie_press_get_meta( 'moxie_press_rating' ) === '4' ) ? 'selected' : '' ?>>4</option>
+        <option <?php echo (moxie_press_get_meta( 'moxie_press_rating' ) === '5' ) ? 'selected' : '' ?>>5</option>
       </select>
     </p>
 
     <p>
-      <label for="moxie_movies_year"><b><?php _e( 'Year', TEXT_DOMAIN ); ?></b></label><br>
+      <label for="moxie_press_year"><b><?php _e( 'Year', TEXT_DOMAIN ); ?></b></label><br>
       <i>The year the movie was published</i></label><br>
-      <select name="moxie_movies_year" id="moxie_movies_year"> <?php
+      <select name="moxie_press_year" id="moxie_press_year"> <?php
         for($year = 2016; $year > 1900; $year--){ ?>
-          <option <?php echo (moxie_movies_get_meta( 'moxie_movies_year' ) === $year ) ? 'selected' : '' ?>><?php echo $year ?></option><?php;
+          <option <?php echo (moxie_press_get_meta( 'moxie_press_year' ) === $year ) ? 'selected' : '' ?>><?php echo $year ?></option><?php;
         } ?>
       </select>
     </p>
 
     <p>
-      <label for="moxie_movies_description"><b><?php _e( 'Description', TEXT_DOMAIN ); ?></b><br>
+      <label for="moxie_press_description"><b><?php _e( 'Description', TEXT_DOMAIN ); ?></b><br>
       <i>Short description. Accepts HTML tags.</i></label><br>
-      <textarea name="moxie_movies_description" id="moxie_movies_description" cols="50"><?php echo moxie_movies_get_meta( 'moxie_movies_description' ); ?></textarea>
+      <textarea name="moxie_press_description" id="moxie_press_description" cols="50"><?php echo moxie_press_get_meta( 'moxie_press_description' ); ?></textarea>
     </p><?php
   }
 
   // save custom fields
-  function moxie_movies_save( $post_id ) {
+  function moxie_press_save( $post_id ) {
     if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
-    if ( ! isset( $_POST['moxie_movies_nonce'] ) || ! wp_verify_nonce( $_POST['moxie_movies_nonce'], '_moxie_movies_nonce' ) ) return;
+    if ( ! isset( $_POST['moxie_press_nonce'] ) || ! wp_verify_nonce( $_POST['moxie_press_nonce'], '_moxie_press_nonce' ) ) return;
     if ( ! current_user_can( 'edit_post', $post_id ) ) return;
 
-    if ( isset( $_POST['moxie_movies_poster_url'] ) )
-      update_post_meta( $post_id, 'moxie_movies_poster_url', esc_attr( $_POST['moxie_movies_poster_url'] ) );
-    if ( isset( $_POST['moxie_movies_rating'] ) )
-      update_post_meta( $post_id, 'moxie_movies_rating', esc_attr( $_POST['moxie_movies_rating'] ) );
-    if ( isset( $_POST['moxie_movies_year'] ) )
-      update_post_meta( $post_id, 'moxie_movies_year', esc_attr( $_POST['moxie_movies_year'] ) );
-    if ( isset( $_POST['moxie_movies_description'] ) )
-      update_post_meta( $post_id, 'moxie_movies_description', esc_attr( $_POST['moxie_movies_description'] ) );
+    if ( isset( $_POST['moxie_press_poster_url'] ) )
+      update_post_meta( $post_id, 'moxie_press_poster_url', esc_attr( $_POST['moxie_press_poster_url'] ) );
+    if ( isset( $_POST['moxie_press_rating'] ) )
+      update_post_meta( $post_id, 'moxie_press_rating', esc_attr( $_POST['moxie_press_rating'] ) );
+    if ( isset( $_POST['moxie_press_year'] ) )
+      update_post_meta( $post_id, 'moxie_press_year', esc_attr( $_POST['moxie_press_year'] ) );
+    if ( isset( $_POST['moxie_press_description'] ) )
+      update_post_meta( $post_id, 'moxie_press_description', esc_attr( $_POST['moxie_press_description'] ) );
   }
-  add_action( 'save_post', 'moxie_movies_save' );
+  add_action( 'save_post', 'moxie_press_save' );
 
 }
 
