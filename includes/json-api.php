26f577317c4a4f5e35be92ce4578b006303d7f20
Camilo Delvasto <camilodelvasto@gmail.com>
2016-01-29 11:23:59 -0500

    improved redirection rules, now single posts can be accessed individually using /movies.json/movie-slug && now standard WP functionality for /movies and /movies/movie-slug is preserved

--- includes/json-api.php
+++ includes/json-api.php
@@ -2,21 +2,24 @@
 **************************************************************************
 
 File:         json-api.php
-What it does: creates an api endpoint for movies cpt including custom fields
+What it does: creates an api endpoint for custom field "Movies" under /movies.json && /movies.json/movie-slug
 **************************************************************************/
 
 // add action
 add_action( 'init', 'moxie_press_endpoint' );
 
 function moxie_press_endpoint() {
-    add_rewrite_tag( '%moxie_press%', '([^&]+)' );
-    add_rewrite_rule( 'movies.json/?', 'index.php?moxie_press=a', 'top' );
-    // todo: fix it so that params can be added
-//  add_rewrite_rule( 'movies.json/([^&]+)/?', 'index.php?moxie_press=$matches[1]', 'top' );
+    add_rewrite_tag( '%json%', '([^&]+)' );
+    // rewrite rule to allow single movies to be retrieved using /movies.json/movie-slug
+    add_rewrite_rule( 'movies.json/([^&]+)', 'index.php?json=true&post_type=movie&name=$matches[1]', 'top' );
+    // rewrite rule to allow movie archive to be retrieved using /movies.json
+    add_rewrite_rule( 'movies.json', 'index.php?post_type=movie&json=true', 'top' );
+    // todo: consider more params
 }
 
 
 register_activation_hook( __FILE__, 'moxie_press_rewrite_activation' );
+
 function moxie_press_rewrite_activation()
 {
     flush_rewrite_rules();
@@ -25,19 +28,26 @@ function moxie_press_rewrite_activation()
 function moxie_press_endpoint_data() {
  
     global $wp_query;
- 
-    $movie = $wp_query->get( 'moxie_press' );
- 
-    if ( ! $movie ) {
+
+    // get query vars 
+    $json = $wp_query->get( 'json' );
+    $name = $wp_query->get( 'name' );
+
+    // use this template redirect only if json is requested 
+    if ( $json  != 'true' ) {
         return;
     }
- 
+
     $movie_data = array();
  
+    // default args
     $args = array(
         'post_type'      => 'movie',
         'posts_per_page' => 100,
     );
+    if($name != '') $args['name'] = $name;
+
+    // perform the query
     $movie_query = new WP_Query( $args );
     if ( $movie_query->have_posts() ) : while ( $movie_query->have_posts() ) : $movie_query->the_post();
       $id = get_the_ID();
@@ -51,12 +61,12 @@ function moxie_press_endpoint_data() {
       );
     endwhile; wp_reset_postdata(); endif;
  
+    // send json data using built-in WP function
     wp_send_json( array('data' => $movie_data) );
  
 }
+
+// add the template redirect. notice the custom fields here are plugin-related, so it has to be tweaked for other post types
 add_action( 'template_redirect', 'moxie_press_endpoint_data' );
-
-
-
 
 ?>
