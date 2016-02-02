ccbc212d68e3afc6512147681d396ba49c87b2ec
Camilo Delvasto <camilodelvasto@gmail.com>
2016-02-01 19:06:51 -0500

    caching added using custom identifier for each query

--- README.md
+++ README.md
@@ -1,9 +1,29 @@
 #MoxiePress
 
-A simple but solid plugin for WordPress that does three things:
+A modern plugin for WordPress that will display your movie collection using a shortcode: `[moxie-press]`.
 
-1. Creates a custom post type (for now defined as movies, with fixed custom fields)
-2. Creates an API endpoint like /movies.json 
-3. Produces a beautiful and fancy shortcode for WordPress that uses the API and can be activated on the front or other pages by inserting [moxie-press] in the content
+The plugin does the following:
 
+1. Creates a custom post type with custom fields for title, poster image, year, rating, description and The Movie Database ID
+2. Creates an API endpoint like `/movies.json` and `/moxies.json/movie-name`, caching the request and clearing the cache on post save
+3. Displays the movie collection on any page via the shortcode `[moxie-press]`, showing movie data and a movie trailer if TMDb ID is provided and if there's a related video on the [www.themoviedb.org](www.themoviedb.org)
+
+The plugin can be further enhanced by:
+
+- Creating an options page and adding other themes
+- Adding more parameters to the shortcode call
+- Lazy loading of images
+- Pagination and loading of pages on scroll
+- Pre compilation of handlebar templates for production
+
+##For developers only
+
+The plugin uses the following files:
+
+- **moxie-press-php**: main file, custom post type and field definitions and calls to methods on the other files
+- **json-api.php**: rewrite tags and redirection rules, wp query based on arguments, caching
+- **shortcode.php**: template (handlebars) inclusion, assets inclusion of styles and scripts for the current theme, masonry
+- masonry folder: sass, js files, vendor scripts (masonry) and handlebar templates
+
+Gulpfile.js is included, but you can use grunt or whatever for preprocessing.
 
--- includes/json-api.php
+++ includes/json-api.php
@@ -25,6 +25,7 @@ function moxie_press_rewrite_activation()
     flush_rewrite_rules();
 }
 
+// generate json response based on current query. build and clean cache if needed.
 function moxie_press_endpoint_data() {
  
     global $wp_query;
@@ -38,6 +39,7 @@ function moxie_press_endpoint_data() {
         return;
     }
 
+    // build the query
     $movie_data = array();
  
     // default args
@@ -45,11 +47,16 @@ function moxie_press_endpoint_data() {
         'post_type'      => 'movie',
         'posts_per_page' => 100,
     );
-    if($name != '') $args['name'] = $name;
+    if($name != '') $args['name'] = $name; // add name if provided in query
 
-    // perform the query
-    $movie_query = new WP_Query( $args );
-    if ( $movie_query->have_posts() ) : while ( $movie_query->have_posts() ) : $movie_query->the_post();
+    // check if this particular request is cached, if not, perform the query
+    if ( false === ( $moxie_cached_request = get_transient( 'moxie_cached_request_' . json_encode($args) ) ) ) {
+      $moxie_cached_request = new WP_Query( $args );
+      set_transient( 'moxie_cached_request_' . json_encode($args), $moxie_cached_request );
+    }
+
+    // prepare the object we want to send as response
+    if ( $moxie_cached_request->have_posts() ) : while ( $moxie_cached_request->have_posts() ) : $moxie_cached_request->the_post();
       $id = get_the_ID();
       $movie_data[] = array(
           'id' => $id,
@@ -65,6 +72,13 @@ function moxie_press_endpoint_data() {
     // send json data using built-in WP function
     wp_send_json( array('data' => $movie_data) );
  
+}
+
+// remove the transients upon save. add the action on save
+add_action( 'save_post_movie', 'edit_term_delete_transient' );
+function edit_term_delete_transient() {
+  global $wpdb;
+  $wpdb->query( "DELETE FROM `$wpdb->options` WHERE `option_name` LIKE ('_transient_moxie_cached_request_%')" );
 }
 
 // add the template redirect. notice the custom fields here are plugin-related, so it has to be tweaked for other post types
